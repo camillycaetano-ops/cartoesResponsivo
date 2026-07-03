99277174318ab73f8b4863eb62f96da2edba3a9a
camillycaetano-ops <camilly.caetano@escola.pr.gov.br>
2026-07-03 12:47:14 +0000

    cartoes responsivo

---
+++ script.js
@@ -0,0 +1,28 @@
+const botao1 = document.getElementById("botao1");
+const botao2 = document.getElementById("botao2");
+const botao3 = document.getElementById("botao3");
+const botao4 = document.getElementById("botao4");
+const botao5 = document.getElementById("botao5");
+
+const caixa = document.getElementById("caixa-cartoes");
+
+
+botao1.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaa");
+});
+
+botao2.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaa");
+});
+
+botao3.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaa");
+});
+
+botao4.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaa");
+});
+
+botao5.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaa");
+});
